61b859e8c852de1e3d3cb3c6ba343b3ea10f0d1f
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-29 17:58:55 -0700

    create homepage and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
+// components
+import Sidebar from "./components/Sidebar";
 
+// pages
+import Homepage from "./pages/Homepage";
+
+function App() {
   return (
-    <>
-    
-    </>
+    <div className="flex">
+      <Sidebar />
+      <Homepage />
+    </div>
   );
 }
 
-export default App
+export default App;
