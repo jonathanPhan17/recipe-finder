1d6f859982e3e1321664bd24b0117d0767d9b18a
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-30 13:03:14 -0700

    create search bar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,29 @@
+import { Search } from "lucide-react";
 import React from "react";
 
 const Homepage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2">
+            <Search size={24} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="Search for recipe"
+            />
+          </label>
+        </form>
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular Choices
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
