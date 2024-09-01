e234cae3bf77754e98925b33689251f7f0bbdfc2
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-01 15:29:29 -0700

    add api for dp rendering

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Heart, HeartPulse, Soup } from "lucide-react";
 
-const RecipeCard = () => {
+const getTwoValuesFromHealthLabels = (label) => {
+  return [label[0], label[1]];
+};
+
+const RecipeCard = ({ recipe }) => {
+  const healthLabels = getTwoValuesFromHealthLabels(recipe.healthLabels);
+
   return (
     <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
       <a href="#" className="relative h-32">
         <img
-          src="./public/1.jpg"
+          src={recipe.image}
           alt="recipe img"
           className="rounded-md w-full h-full object-cover cursor-pointer"
         />
         <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-          <Soup size={16} />4 Servings
+          <Soup size={16} />
+          {recipe.yield} Servings
         </div>
         <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
           <Heart
@@ -21,22 +28,25 @@ const RecipeCard = () => {
         </div>
       </a>
       <div className="flex mt-1">
-        <p className="font-bold tracking-wide">Roasted Chicken</p>
+        <p className="font-bold tracking-wide">{recipe.label}</p>
       </div>
-      <p className="my-2">Turkish Chicken</p>
+      <p className="my-2">
+        {recipe.cuisineType[0].charAt(0).toUpperCase() +
+          recipe.cuisineType[0].slice(1)}{" "}
+        Cuisine
+      </p>
       <div className="flex gap-2 mt-auto">
-        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-          <HeartPulse size={16} />
-          <span className="text-sm tracking-tighter font-semibold">
-            Gluten-free
-          </span>
-        </div>
-        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-          <HeartPulse size={16} />
-          <span className="text-sm tracking-tighter font-semibold">
-            Heart-healthy
-          </span>
-        </div>
+        {healthLabels.map((label, index) => (
+          <div
+            key={index}
+            className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md"
+          >
+            <HeartPulse size={16} />
+            <span className="text-sm tracking-tighter font-semibold">
+              {label}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
